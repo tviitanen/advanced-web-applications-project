f8f2a7fe2d510b749f1d694ba51c20b5f0e7064c
Teemu Viitanen <teemu.viitanen@gmail.com>
2023-03-05 16:52:52 +0200

    removed unused import

--- client/src/components/Data.js
+++ client/src/components/Data.js
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 
 function Data() {
   const { id } = useParams();
