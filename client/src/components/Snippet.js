dc4476c2092b8a5f6b9cb5eebbd5899ca0ad6599
Teemu Viitanen <teemu.viitanen@gmail.com>
2023-03-07 12:41:09 +0200

    fixed useEffect loop

--- client/src/components/Snippet.js
+++ client/src/components/Snippet.js
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import "highlight.js/styles/github.css";
 import hljs from "highlight.js";
 
@@ -15,7 +15,8 @@ function Snippet() {
 
   // Fetch post
   useEffect(() => {
-    async function getPost() {
+    async function getSnippet() {
+      console.log(id);
       fetch(`http://localhost:4000/api/${id}`, {
         method: "GET",
         headers: {
@@ -26,13 +27,34 @@ function Snippet() {
         .then((response) => response.json())
         .then((data) => {
           console.log("Success:", data);
-          setData(data.post);
+          console.log(data.snippet.title);
+          setData(data);
         })
         .catch((error) => {
           console.error("Error:", error);
         });
     }
-    getPost();
+    getSnippet();
   }, [id]);
+
+  // Don't render until we have data
+  if (data === null || data === undefined) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="container">
+      <div key={""} className="row">
+        <div className="col s12">
+          <div className="card grey darken-3 ">
+            <div className="card-content white-text">
+              <span className="card-title">{data.snippet.title}</span>
+              <p>{}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Snippet;
